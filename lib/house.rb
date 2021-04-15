c27a775a8241eea13ea8cceec4513feedbb5ba26
Shayla Lane <lanesha@oregonstate.edu>
2021-04-15 10:31:07 -0700

    Add case for line 5.

--- lib/house.rb
+++ lib/house.rb
@@ -8,8 +8,10 @@ class House
             "This is the malt that lay in the house that Jack built.\n"
         when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
-        else
+        when 4
             "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        else
+            "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
         
     end
