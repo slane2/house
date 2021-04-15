0837d24b6b51203664226fb513f4e7003179b80d
Shayla Lane <lanesha@oregonstate.edu>
2021-04-15 10:28:28 -0700

    Add case statement with two options.

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,13 @@
 class House
 
     def line(num)
-        "This is the house that Jack built.\n"
+        case num
+        when 1
+            "This is the house that Jack built.\n"
+        else
+            "This is the malt that lay in the house that Jack built.\n"
+        end
+        
     end
 
 end
