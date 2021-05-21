0fdaeefbfb5b3d448bcd9c238e899a10f5ddbc9a
Shayla Lane <lanesha@oregonstate.edu>
2021-05-20 21:11:43 -0700

    Add eighth line to lines and replace in case 8.

--- lib/house.rb
+++ lib/house.rb
@@ -8,7 +8,8 @@ class House
             "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
             "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
             "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-            "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
+            "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         ]
     end
 
@@ -29,7 +30,7 @@ class House
         when 7
             lines[6]
         when 8
-            "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            lines[7]
         when 9
             "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 10
