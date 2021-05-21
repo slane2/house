b8a77da5328d8c931a025802c23eabd9ed50b805
Shayla Lane <lanesha@oregonstate.edu>
2021-05-21 13:51:03 -0700

    Add case 2 to phrase and remove from line.

--- lib/house.rb
+++ lib/house.rb
@@ -1,15 +1,14 @@
 class House
     def phrase(num)
         case num
-        when 1
+        when 2
+            "malt that lay"
+        else
             ""
         end
     end
-
     def line(num)
         case num
-        when 2
-            "This is the malt that lay in the house that Jack built.\n"
         when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
         when 4
@@ -31,7 +30,7 @@ class House
         when 12
             "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
-            "This is the #{phrase(num)} house that Jack built."
+            "This is the #{phrase(num)} in the house that Jack built."
         end
     end
 
