0a8e73ac20b10c44758294d529700d425094c7b2
Shayla Lane <lanesha@oregonstate.edu>
2021-05-20 21:01:59 -0700

    Add first line to lines and replace in case 1.

--- lib/house.rb
+++ lib/house.rb
@@ -2,14 +2,14 @@ class House
 
     def lines
         [
-            
+            "This is the house that Jack built.\n"
         ]
     end
 
     def line(num)
         case num
         when 1
-            "This is the house that Jack built.\n"
+            lines[0]
         when 2
             "This is the malt that lay in the house that Jack built.\n"
         when 3
