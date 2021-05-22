498641ae757844c7317225e1ae6017d4f0218ab3
Shayla Lane <lanesha@oregonstate.edu>
2021-05-21 17:08:15 -0700

    Add case 8 to phrase and remove from line.

--- lib/house.rb
+++ lib/house.rb
@@ -13,14 +13,13 @@ class House
             "the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         when 7
             "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+        when 8
+            "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         else
-            ""
         end
     end
     def line(num)
         case num
-        when 8
-            "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 9
             "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 10
