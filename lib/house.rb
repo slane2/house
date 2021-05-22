40755c6af4ca399a7a730944d4d6428bb9dac3b1
Shayla Lane <lanesha@oregonstate.edu>
2021-05-21 17:30:11 -0700

    Make else statement join method.

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,6 @@
 class House
     def phrase(num)
+        lines = [""]
         case num
         when 2
             "the malt that lay in "
@@ -24,7 +25,7 @@ class House
         when 12
             "the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         else
-            ""
+            lines.last(num).join(" ")
         end
     end
     def line(num)
