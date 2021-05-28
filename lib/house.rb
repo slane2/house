70c0eb5ff8dee7e117f5ec7d25cc0f0ea02fffe6
Shayla Lane <lanesha@oregonstate.edu>
2021-05-27 17:35:30 -0700

    Make lines in phrase function line segments instead of the full line and remove cases.

--- lib/house.rb
+++ lib/house.rb
@@ -6,48 +6,21 @@ class House
 
     def phrase(num)
         lines = [
-            "",
-            "the malt that lay in ",
-            "the rat that ate the malt that lay in ",
-            "the cat that killed the rat that ate the malt that lay in ",
-            "the dog that worried the cat that killed the rat that ate the malt that lay in ",
-            "the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
-            "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
-            "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
-            "the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
-            "the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
-            "the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
-            "the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+            "the horse and the hound and the horn that belonged to",
+            "the farmer sowing his corn that kept",
+            "the rooster that crowed in the morn that woke",
+            "the priest all shaven and shorn that married",
+            "the man all tattered and torn that kissed",
+            "the maiden all forlorn that milked",
+            "the cow with the crumpled horn that tossed",
+            "the dog that worried",
+            "the cat that killed",
+            "the rat that ate",
+            "the malt that lay in",
+            ""
         ]
 
-        case num
-        when 1
-            lines[num - 1]
-        when 2
-            lines[num - 1]
-        when 3
-            lines[num - 1]
-        when 4
-            lines[num - 1]
-        when 5
-            lines[num - 1]
-        when 6
-            lines[num - 1]
-        when 7
-            lines[num - 1]
-        when 8
-            lines[num - 1]
-        when 9
-            lines[num - 1]
-        when 10
-            lines[num - 1]
-        when 11
-            lines[num - 1]
-        when 12
-            lines[num - 1]
-        else
-            lines.last(num).join(" ")
-        end
+        lines.last(num).join(" ")
     end
     
     def line(num)
@@ -66,19 +39,19 @@ class Pirate < House
     end
 end
 
-class Random < House
+class RandomHouse < House
     chunks = [
-        "",
-        "the malt that lay in ",
-        "the rat that ate the ",
-        "the cat that killed the ",
-        "the dog that worried the ",
-        "the cow with the crumpled horn that tossed the ",
-        "the maiden all forlorn that milked the ",
-        "the man all tattered and torn that kissed the ",
-        "the priest all shaven and shorn that married the ",
-        "the rooster that crowed in the morn that woke the ",
+        "the horse and the hound and the horn that belonged to the ",
         "the farmer sowing his corn that kept the ",
-        "the horse and the hound and the horn that belonged to the "
+        "the rooster that crowed in the morn that woke the ",
+        "the priest all shaven and shorn that married the ",
+        "the man all tattered and torn that kissed the ",
+        "the maiden all forlorn that milked the ",
+        "the cow with the crumpled horn that tossed the ",
+        "the dog that worried the ",
+        "the cat that killed the ",
+        "the rat that ate the ",
+        "the malt that lay in ",
+        ""
     ]
 end
